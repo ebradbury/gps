e2a803c34337959c52a55de85e3192140b81c5c0
Elliot Bradbury <ebradbury@gmail.com>
2024-10-02 09:58:13 -0400

    moving to tcp server

--- server/proxy.js
+++ server/proxy.js
@@ -1,14 +1,17 @@
 const WebSocket = require('ws');
 const net = require('net');
 
-const wss = new WebSocket.Server({ port: 3000 });
+const lastMessage = null;
 
-// GPSD server connection details
-const gpsdHost = 'localhost';
-const gpsdPort = 2947;
+const wsServer = new WebSocket.Server({ port: 3000 });
+const wsClients = new Set();
 
-wss.on('connection', (ws) => {
-    console.log('WebSocket connection established');
+const tcpServer = new net.Server({ post: 4000 });
+const tcpClients = new Set();
+
+wsServer.on('connection', (ws) => {
+    console.log('WebSocket connection received');
+    wsClients.add(ws);
 
     // Connect to gpsd over TCP
     const gpsdSocket = new net.Socket();
@@ -21,24 +24,44 @@ wss.on('connection', (ws) => {
         }, 1000);
     });
 
-    // When WebSocket message is received from the browser
+    // When WebSocket message is received from the browser, pass it along 
     ws.on('message', (message) => {
         console.log('Received from WebSocket:', message.toString());
+        lastMessage = message.toString();
 
-        // Send the data to gpsd over TCP
-        gpsdSocket.write(message.toString());
+        tcpClients.forEach(client => {
+            console.log('Sending last message to TCP client');
+            client.write(lastMessage);
+        });
     });
 
+    // Clean up
+    ws.on('close', () => {
+        console.log('WebSocket client disconnected')
+        wsClients.delete(ws);
+    });
+});
+
+tcpServer.on('connection', (tcpClient) => {
+    console.log('TCP connected received');
+    tcpClients.add(tcpClient);
+
     // Handle gpsd data
-    gpsdSocket.on('data', (data) => {
+    tcpServer.on('data', (data) => {
         console.log('Received from gpsd:', data.toString());
 
         // Optionally send back to the WebSocket client
         ws.send(data.toString());
     });
 
-    // Clean up
-    ws.on('close', () => {
-        gpsdSocket.end();
+    // Handle connection end
+    tcpServer.on('end', () => {
+        console.log('TCP client disconnected');
+        tcpClients.delete(tcpClient);
     });
+
+    tcpClient.write('?WATCH={"enable":true,"json":true,"nmea":true}\n');
+    if(lastMessage) {
+        tcpClient.write(lastMessage);
+    }
 });
