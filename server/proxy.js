e35fb54b30b1e5d9e393f641d5d94c3d7fe52384
Elliot Bradbury <ebradbury@gmail.com>
2024-10-02 10:32:26 -0400

    yep

--- server/proxy.js
+++ server/proxy.js
@@ -6,9 +6,9 @@ var lastMessage = null;
 const wsServer = new WebSocket.Server({ port: 3000 });
 const wsClients = new Set();
 
-const tcpServer = new net.Server();
+const tcpServer = new net.createServer();
 const tcpClients = new Set();
-tcpServer.listen(4000, '127.0.0.1');
+tcpServer.listen(4000, '127.0.0.1', () => console.log('TCP server listening on port 4000'));
 
 wsServer.on('connection', (ws) => {
     console.log('WebSocket connection received');
